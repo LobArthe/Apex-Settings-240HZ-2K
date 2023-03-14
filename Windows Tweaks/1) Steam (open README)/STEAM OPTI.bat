1e55e1840c46960003ebda9546fea71f094ff55b
LobArthe <119135536+LobArthe@users.noreply.github.com>
2023-03-14 13:37:04 +0100

    Add files via upload

---
+++ Windows Tweaks/1) Steam (open README)/STEAM OPTI.bat
@@ -0,0 +1,1 @@
+start steam.exe -dev -console -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264 -cef-force-32bit -no-cef-sandbox -vrdisable -cef-disable-breakpad
